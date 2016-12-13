3272ffaeb95aab2e5bf2b74fe84ca5b5fcbd6f47
michael dudley <michael.dudley@improvingenterprises.com>
2016-12-13 16:42:18 -0600

    Initial Commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FooTest : FooTestBase
+    {
+        protected override bool Return_true()
+        {
+            return true;
+        }
+
+        protected override string Return_hello_world()
+        {
+            return "Hello World";
+        }
+    }
+
+    public abstract class FooTestBase
+    {
+        [TestMethod]
+        public void Execute_Return_true()
+        {
+            Assert.IsTrue(Return_true());
+        }
+
+        [TestMethod]
+        public void Execute_Return_Hello_World()
+        {
+            Assert.AreEqual("Hello World", Return_hello_world());
+        }
+
+        protected abstract bool Return_true();
+
+        protected abstract string Return_hello_world();
+    }
+}
